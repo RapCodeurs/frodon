75f6bc6f155cd9643e62484101c6764a1abe44d2
RapCodeurs <maureljoumessi9@gmail.com>
2025-11-12 17:14:46 +0100

    modified:   userdata/tomcat_ubuntu.sh

--- userdata/tomcat_ubuntu.sh
+++ userdata/tomcat_ubuntu.sh
@@ -2,4 +2,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-17-jdk -y
-sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
+sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git wget -y
